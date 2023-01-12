6108f47d8214872daf7e97167d999f9d838eb252
SHERlocked93 <443033099@qq.com>
2023-01-12 09:16:40 +0800

    feat: 引入cJSON

---
+++ tests/read/demo1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by qiany on 2023/1/11.
+//
+
+
+#include <cjson/cJSON.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "hello world!" << endl;
+    
+    return 0;
+}
